9916817cf28ddcc8324c4b85192e438a35b9da2b
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2024-09-29 22:49:18 +0200

    added other map error cases

--- src/errors.c
+++ src/errors.c
@@ -6,7 +6,7 @@
 /*   By: tomek <tomek@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 17:53:44 by tomek             #+#    #+#             */
-/*   Updated: 2024/09/29 18:50:38 by tomek            ###   ########.fr       */
+/*   Updated: 2024/09/29 22:19:17 by tomek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,20 @@ void	error_msg(int error_nbr)
 	{
 		ft_printf("Error\n");
 		ft_printf("You have to pass only one map!\n");
+	}
+	if (error_nbr == 2)
+	{
+		ft_printf("Error\n");
+		ft_printf("Something's wrong with the map file!\n");
+	}
+	if (error_nbr == 3)
+	{
+		ft_printf("Error\n");
+		ft_printf("Something's wrong with map file format!\n");
+	}
+	if (error_nbr == 4)
+	{
+		ft_printf("Error\n");
+		ft_printf("Something's wrong with the map itself!\n");
 	}
 }
