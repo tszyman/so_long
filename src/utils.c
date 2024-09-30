aa9f82881543e1b41bc22d3ff6f0163ccdd1819b
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2024-09-30 23:26:47 +0200

    typo

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: tomek <tomek@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 21:30:32 by tomek             #+#    #+#             */
-/*   Updated: 2024/09/30 21:37:03 by tomek            ###   ########.fr       */
+/*   Updated: 2024/09/30 23:08:07 by tomek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	count_lines(t_so_long *so_long)
 	if (so_long->map_fd < 0)
 		return (0);
 	line = get_next_line(so_long->map_fd);
-	while (line != NULL);
+	while (line != NULL)
 	{
 		count_lines++;
 		free(line);
