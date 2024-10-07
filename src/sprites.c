7a38a22731c08f8b668fc8c05448a62689eb1ea3
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2024-10-07 22:27:38 +0200

    new sprites added

--- src/sprites.c
+++ src/sprites.c
@@ -6,7 +6,7 @@
 /*   By: tomek <tomek@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 13:39:56 by tomek             #+#    #+#             */
-/*   Updated: 2024/10/06 19:48:00 by tomek            ###   ########.fr       */
+/*   Updated: 2024/10/07 22:25:03 by tomek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static void	load_idle_sprites(t_so_long *so_long, int *width, int *height)
 	so_long->sprites[0] = mlx_xpm_file_to_image(so_long->mlx,
 			"./sprites/wall.xpm", width, height);
 	so_long->sprites[1] = mlx_xpm_file_to_image(so_long->mlx,
-			"./sprites/item_close.xpm", width, height);
+			"./sprites/item_fish.xpm", width, height);
 	so_long->sprites[2] = mlx_xpm_file_to_image(so_long->mlx,
 			"./sprites/item_open.xpm", width, height);
 	so_long->sprites[3] = mlx_xpm_file_to_image(so_long->mlx,
@@ -25,9 +25,9 @@ static void	load_idle_sprites(t_so_long *so_long, int *width, int *height)
 	so_long->sprites[4] = mlx_xpm_file_to_image(so_long->mlx,
 			"./sprites/gate_open.xpm", width, height);
 	so_long->p->idle_right = mlx_xpm_file_to_image(so_long->mlx,
-			"./sprites/right.xpm", width, height);
+			"./sprites/cat_right.xpm", width, height);
 	so_long->p->idle_left = mlx_xpm_file_to_image(so_long->mlx,
-			"./sprites/left.xpm", width, height);
+			"./sprites/cat_left.xpm", width, height);
 }
 
 static int	check_sprites_loaded(t_so_long *so_long)
