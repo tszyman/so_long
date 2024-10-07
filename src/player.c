5eaa171a76c69bb9340d0accfbdcc3ea4a65538d
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2024-10-07 22:28:03 +0200

    sprite disappears after being eaten

--- src/player.c
+++ src/player.c
@@ -6,7 +6,7 @@
 /*   By: tomek <tomek@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 17:56:30 by tomek             #+#    #+#             */
-/*   Updated: 2024/10/06 19:58:52 by tomek            ###   ########.fr       */
+/*   Updated: 2024/10/07 22:20:48 by tomek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ static void	handle_item(t_so_long *so_long, int new_x_pos, int new_y_pos)
 	if (so_long->map[new_y_pos][new_x_pos] == 'C')
 	{
 		so_long->items_total--;
-		so_long->map[new_y_pos][new_x_pos] = 'c';
+		so_long->map[new_y_pos][new_x_pos] = '0';
 		if (so_long->items_total == 0)
 		{
 			ft_printf("All items collected! \n");
